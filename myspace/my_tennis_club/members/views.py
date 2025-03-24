c2d5798156fc76dee93f392125d9a4a8bda445b7
Osathi Jantrasri <otfor01@gmail.com>
2025-03-24 15:54:17 +0700

    Add members template and update view to display member details

--- myspace/my_tennis_club/members/views.py
+++ myspace/my_tennis_club/members/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    # template = loader.get_template('myfirst.html')
+    # return HttpResponse(template.render())
+    
+    mymembers = Member.objects.all().values() # get all members from model
+    template = loader.get_template('all_members.html')
+    # pass the members to the template
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
