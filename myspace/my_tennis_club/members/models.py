1c101a677ccbb939a9ddd2f8dd0827dd8649e130
Osathi Jantrasri <otfor01@gmail.com>
2025-03-24 15:43:58 +0700

    add model and note in obsidian how to manage data

--- myspace/my_tennis_club/members/models.py
+++ myspace/my_tennis_club/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    note = models.CharField(max_length=100)
+    bday = models.DateField()
+    email = models.EmailField(null=True) # can be null
