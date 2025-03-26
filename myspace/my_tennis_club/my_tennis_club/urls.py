c5308bf63bf661d51b29ad18a63755f219aecf03
Osathi Jantrasri <otfor01@gmail.com>
2025-03-26 14:04:00 +0700

    Refactor Todo functionality into separate serializers and views

--- myspace/my_tennis_club/my_tennis_club/urls.py
+++ myspace/my_tennis_club/my_tennis_club/urls.py
@@ -16,29 +16,21 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers, serializers, viewsets
-from todoapp.models import Todo
+from todoapp import views
 from django.contrib.auth.models import User
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ['title', 'description', 'completed']
         
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
         
-class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
 router = routers.DefaultRouter()
-router.register(r'todos', TodoViewSet)
+router.register(r'todos', views.TodoViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
--- myspace/my_tennis_club/todoapp/views.py
+++ myspace/my_tennis_club/todoapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
