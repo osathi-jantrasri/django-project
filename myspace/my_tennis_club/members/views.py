6ef5526de9fb6bc5eaff1007bebdbce6f4980265
Osathi Jantrasri <otfor01@gmail.com>
2025-03-26 14:49:27 +0700

    Update permission classes for Member and Todo viewsets to AllowAny

--- myspace/my_tennis_club/members/views.py
+++ myspace/my_tennis_club/members/views.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions, viewsets
 from .models import Member
 from .serializers import MemberSerializer
+from rest_framework.permissions import AllowAny
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [AllowAny]
 
--- myspace/my_tennis_club/todoapp/views.py
+++ myspace/my_tennis_club/todoapp/views.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions, viewsets
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    # permission_classes = [permissions.NOT]
+    permission_classes = [AllowAny]
